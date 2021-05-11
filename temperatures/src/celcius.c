db669e8ff3d3560fd416d808b553a4720cd871d0
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-05-11 11:12:09 -0400

    for loop and formated printing

---
+++ temperatures/src/celcius.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main()
+{
+    float fahr, celcius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celcius = lower;
+    // print header table
+    printf("celcius | fahrenheit\n--------------------\n");
+
+    while (celcius <= upper)
+    {
+        fahr = 1.8 * celcius + 32.0;
+        printf("%7.3f | %10.3f\n", celcius, fahr);
+        celcius = celcius + step;
+    }
+}
