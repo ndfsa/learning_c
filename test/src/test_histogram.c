8ad25991bea4767f4898da8a6d3b9282556a9799
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-08-06 23:56:53 -0400

    histogram now works with both functions

--- test/src/test_histogram.c
+++ test/src/test_histogram.c
@@ -1,33 +1,21 @@
 #include "histogram.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 main()
 {
 	int counter1[50];
-	char buf[1024];
-	int i;
-	char c;
-
-	for (i = 0; i < 50; i++)
-	{
-		counter1[i] = 0;
-	}
-	i = 0;
-	while ((c = getchar()) != EOF)
-	{
-		if (i == 1023)
-		{
-			word_length_count(buf, counter1, 50);
-			i = 0;
-		}
-		buf[i++] = c;
-	}
-	if (i < 1023)
-	{
-		buf[i] = '\0';
-		word_length_count(buf, counter1, 50);
-	}
+	int counter2[128];
+	memset(counter1, 0, sizeof(int) * 50);
+	word_length_count("hello world this is a test woohoo", counter1, 50);
 	histogram(counter1, 50);
+	vhistogram(counter1, 50);
+
+	memset(counter2, 0, sizeof(int) * 50);
+	frequency_count("hello world this is another test", counter1);
+	histogram(counter1, 128);
+	vhistogram(counter1, 128);
+
 	return 0;
 }
