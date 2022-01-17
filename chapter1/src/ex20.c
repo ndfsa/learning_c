edf0aa095059753b872b5929befa4148435c5bfd
ndfsa <25588077+ndfsa@users.noreply.github.com>
2022-01-16 21:08:28 -0400

    ex20

--- chapter1/src/ex20.c
+++ chapter1/src/ex20.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #define DEFAULT_TS 4
+#define IN 1
+#define OUT 0
 
-main(){
-    /* Write a program detab that replaces tabs in the input with te propper number of blanks to
+main()
+{
+    /* Write a program detab that replaces tabs in the input with the propper number of blanks to
      * space to the next tabs stop. Assume a fixed set of tab stop, say every n columns. Should n
      * be a variable or a symbolic parameter?
      * A. I think it should be a symbolic parameter, as the book does not mention command line
-     * arguments, in which case a variable with a symbolic parameter fallback would be preferable
+     * arguments, in which case a variable with a symbolic parameter fallback would be preferable.
      * */
-    short int offset;
+    int count, offset;
+    int c;
 
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            offset = DEFAULT_TS - (count % 4);
+            printf("%*s", offset, "");
+            count += offset;
+        }
+        else
+            putchar(c);
+
+        if (c == '\n')
+            count = 0;
+        else
+            count++;
+    }
 }
