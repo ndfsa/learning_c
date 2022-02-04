08c6444a6a26af2cc087a8b42e81081bd1afcb30
ndfsa <25588077+ndfsa@users.noreply.github.com>
2022-02-04 00:39:51 -0400

    ex22 and ex23

---
+++ chapter1/src/ex22.c
@@ -0,0 +1,66 @@
+#include "ch1utils.h"
+#include <stdio.h>
+#define MAX_LENGTH 100
+
+main()
+{
+    /* Write a program to "fold" long input lines into two or more shorter lines after the last
+     * non-blank character that occurs before the n-th column of input. Make sure your program does
+     * something intelligent with very long lines, and if there are no blanks or tabs before the
+     * specified column.
+     * */
+
+    int c, count, last_blank, offset;
+    char buffer[MAX_LENGTH + 1];
+    offset = 0;
+    count = 0;
+    clear_buffer(buffer, MAX_LENGTH + 1);
+    last_blank = -1;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (count + offset < MAX_LENGTH)
+        {
+            if (IS_BLANK(c))
+                last_blank = count;
+
+            buffer[count++] = c;
+            continue;
+        }
+
+        if (last_blank != -1)
+        {
+            buffer[last_blank] = '\n';
+            offset = MAX_LENGTH - last_blank - 2;
+            printf("%s", buffer);
+            clear_buffer(buffer, MAX_LENGTH);
+
+            count = 0;
+            buffer[count++] = c;
+            continue;
+        }
+        count = 0;
+        offset = 0;
+        printf("%s", buffer);
+        clear_buffer(buffer, MAX_LENGTH);
+
+        while (1)
+        {
+            if (IS_BLANK(c))
+            {
+                putchar('\n');
+                break;
+            }
+            else if (c == EOF)
+            {
+                return 0;
+            }
+            else
+            {
+                putchar(c);
+            }
+            c = getchar();
+        }
+    }
+    printf("%s", buffer);
+}
---
+++ chapter1/src/ex23.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#define IN 1
+#define OUT 0
+
+main()
+{
+    /* Write a prigram to remove all comments from a C program. Don't forget to handle quoted
+     * strings and character constants properly. C comments do not nest.
+     * */
+    int c, prev, s_state, c_state;
+    prev = -1;
+    s_state = OUT;
+    c_state = OUT;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (s_state == OUT && c == '"')
+        {
+            putchar(c);
+            s_state = IN;
+        }
+        else if (c == '"')
+        {
+            putchar(c);
+            s_state = OUT;
+        }
+        else if (s_state == OUT)
+        {
+            if (c_state == OUT)
+                switch (c)
+                {
+                case '/':
+                    break;
+                case '*':
+                    if (prev == '/')
+                        c_state = IN;
+                    else
+                        putchar(c);
+                    break;
+                default:
+                    if (prev == '/')
+                        putchar(prev);
+                    putchar(c);
+                    break;
+                }
+            else if (c == '/' && prev == '*')
+            {
+                c_state = OUT;
+                continue;
+            }
+        }
+        else
+        {
+            putchar(c);
+        }
+        prev = c;
+    }
+}
